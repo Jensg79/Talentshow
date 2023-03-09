9e5404f91895c16af7eb43f5a1e76c3519224407
Jensg79 <91133582+Jensg79@users.noreply.github.com>
2023-03-09 20:40:15 +0100

    Link zur Startseite

--- Talentshow/Talentshow.py
+++ Talentshow/Talentshow.py
@@ -4,6 +4,7 @@ from pcconfig import config
 import pynecone as pc
 
 ablauf = "http://localhost:3000/ablauf"
+start = "http://localhost:3000"
 docs_url = "http://localhost:3000/ablauf"
 regeln = "http://localhost:3000/regeln"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -100,13 +101,37 @@ def startseite() -> pc.Component:
 def ablauf() -> pc.Component:
     return pc.center(pc.vstack(
         pc.heading("Talentshow 2023", font_size="3em", color='red'),
-        pc.box('Der Ablaufplan', font_size="2em", color='blue')))
+        pc.box('Der Ablaufplan', font_size="2em", color='blue'),
+        pc.link(
+            "Startseite",
+            href='/',
+            border="0.1em solid",
+            padding="0.5em",
+            border_radius="0.5em",
+            _hover={
+                "color": "rgb(107,99,246)",
+            },
+            spacing="1.5em",
+        ),
+    ),)
 
 
 def regeln() -> pc.Component:
     return pc.center(pc.vstack(
             pc.heading("Talentshow 2023", font_size="3em", color='red'),
-            pc.box('Die Regeln für die Jury', font_size="2em", color='blue')))
+            pc.box('Die Regeln für die Jury', font_size="2em", color='blue'),
+            pc.link(
+                "Startseite",
+                href='/',
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                _hover={
+                    "color": "rgb(107,99,246)",
+                },
+            ),
+        spacing="1.5em",)
+    )
 
 app = pc.App()
 app.add_page(startseite, route="/")
